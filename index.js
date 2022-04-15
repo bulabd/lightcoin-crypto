cfbf66a954f85fb739ad010522189ba9506f6287
bulabd <bulabd@outlook.com>
2022-04-15 21:15:21 +0000

    LHL crypto atm solution

--- index.js
+++ index.js
@@ -1,13 +1,60 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
 
-  constructor(amount) {
+  get balance() {
+    let balance = 0;
+    for (let operation of this.transactions) {
+      balance += operation.value;
+    }
+    return balance;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+
+}
+
+class Transaction {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    if (!this.isAllowed()) return false;
+    this.time = new Date();
+    this.account.addTransaction(this);
+    return true;
+  }
+
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -(this.amount);
+  }
+
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0);
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
+  }
+
+  isAllowed() {
+    return true;
   }
 
 }
@@ -17,13 +64,21 @@ class Withdrawal {
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const  myAccount = new Account('snow-patrol');
 
-t1 = new Withdrawal(50.25);
+console.log('Starting Balance: ', myAccount.balance);
+
+const t1 = new Withdrawal(1.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
+console.log('Account Balance: ', myAccount.balance);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Deposit(9.99, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
+console.log('Account Balance: ', myAccount.balance);
 
-console.log('Balance:', balance);
+const t3 = new Withdrawal(9.99, myAccount);
+t3.commit();
+console.log('Account Balance: ', myAccount.balance);
+
+console.log('Ending Balance:', myAccount.balance);
+console.log('Account Transaction History: ', myAccount.transactions);
